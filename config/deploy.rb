fbf9b9f1ad72d132f62d7af66dbac1f738415c70
Katsuyoshi Fujisawa <kfuji@example.jp>
2013-07-10 22:07:57 +0900

    preparation for Capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,15 @@
+require "bundler/capistrano"
+
+set :application, "asagao"
+set :rails_env, "production"
+
+server "example.com", :web, :app, :db, primary: true
+
+set :repository, "ssh://git@git.example.com/var/git/asegao.git"
+set :scm, :git
+set :branch, "master"
+set :user, "asagao"
+set :use_sudo, false
+set :deploy_to, "/home/#{user}/#{rails_env}"
+set :deploy_via, :remote_cache
+ssh_options[:forward_agent] = true
